04e7e8f18ac54a305c5455d9a376dddc39375eb8
Pingu-Pox <brendandaneambrose@gmail.com>
2023-05-11 23:44:32 -0500

    Started work on base command

--- commands/task.js
+++ commands/task.js
@@ -1,5 +1,8 @@
 import { SlashCommandBuilder } from "discord.js";
 import * as add from "./add.js";
+import * as remove from "./remove.js";
+import * as list from "./list.js";
+import * as merge from "./merge.js";
 
 // Creates an Object in JSON with the data required by Discord's API to create a SlashCommand
 const create = () => {
@@ -19,6 +22,12 @@ const invoke = async (interaction) => {
 
     if (subCommand === "add") {
         await add.invoke(interaction);
+    } else if (subcommand === "remove") {
+        await remove.invoke(interaction);
+    } else if (subcommand === "list") {
+        await list.invoke(interaction);
+    } else if (subcommand === "merge") {
+        await merge.invoke(interaction);
     } else {
         interaction.reply({
             content: "Invalid subcommand: " + subCommand,
