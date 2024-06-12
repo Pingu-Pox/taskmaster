c7f787db02db792399e3f8cef92e4f3afee66ec2
Brendan <brendandaneambrose@gmail.com>
2024-06-12 14:52:17 -0500

    Added checks to make sure variables are being read

--- index.js
+++ index.js
@@ -44,6 +44,92 @@ createDirectoryIfNotExists("./data/merges");
 createDirectoryIfNotExists("./data/staged");
 createDirectoryIfNotExists("./data/trash");
 
+const requiredVariables = [
+    "GUILD_ID",
+    "IGC1",
+    "IGC2",
+    "IGC3",
+    "IGC4",
+    "IGC5",
+    "IGC6",
+    "IGC7",
+    "RAC1",
+    "RAC2",
+    "RAC3",
+    "RAC4",
+    "RAC5",
+    "RAC6",
+    "RAC7",
+    "RUC1",
+    "RUC2",
+    "RUC3",
+    "RUC4",
+    "RUC5",
+    "RUC6",
+    "RUC7",
+    "IGL1",
+    "IGL2",
+    "IGL3",
+    "IGL4",
+    "IGL5",
+    "IGL6",
+    "IGL7",
+    "RAL1",
+    "RAL2",
+    "RAL3",
+    "RAL4",
+    "RAL5",
+    "RAL6",
+    "RAL7",
+    "RUL1",
+    "RUL2",
+    "RUL3",
+    "RUL4",
+    "RUL5",
+    "RUL6",
+    "RUL7",
+    "IGM1",
+    "IGM2",
+    "IGM3",
+    "IGM4",
+    "IGM5",
+    "IGM6",
+    "IGM7",
+    "RAM1",
+    "RAM2",
+    "RAM3",
+    "RAM4",
+    "RAM5",
+    "RAM6",
+    "RAM7",
+    "RUM1",
+    "RUM2",
+    "RUM3",
+    "RUM4",
+    "RUM5",
+    "RUM6",
+    "RUM7",
+    "KIN",
+    "THANE",
+    "HIGHTHANE",
+    "IRONGUARD",
+    "RAMHEART",
+    "RUNEFORGE",
+    "AUTHORIZED_ADMINS",
+    "TOKEN",
+];
+
+const missingVariables = requiredVariables.filter(
+    (variable) => !process.env[variable]
+);
+
+if (missingVariables.length > 0) {
+    console.error(
+        `Missing environment variables: ${missingVariables.join(", ")}`
+    );
+    process.exit(1);
+}
+
 // Login with the environment data
 if (process.env.TOKEN === "") {
     console.error(
